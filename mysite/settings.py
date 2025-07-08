a127fe324d1fb1b4da3c4432beb59d6ad33a9587
name-amir <name.amir82@gmail.com>
2025-07-08 17:57:37 +0330

    created render html file

--- mysite/settings.py
+++ mysite/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'mysite.urls'
 
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
--- website/views.py
+++ website/views.py
@@ -3,11 +3,11 @@ from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 
 def index_view(request):
-    return HttpResponse('<h1>Home Page</h1>')
+    return render(request, 'index.html')
 
 def about_view(request):
-    return HttpResponse('<h1>view Page</h1>')
+    return render(request, 'about.html')
 
 def contact_view(request):
-    return HttpResponse('<h1> Contact Page </h1>')
+    return render(request, 'contact')
 # Create your views here.
