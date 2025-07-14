87f516e353661c4eb67217da02d6915a62f35792
name-amir <name.amir82@gmail.com>
2025-07-14 23:30:52 +0330

    content

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from blog.models import Post
 
 # Register your models here.
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('title' ,'counted_views','status','published_date','created_date')
+    list_filter = ('status',)
+    search_fields = ['title','content']
+admin.site.register(Post,PostAdmin)
