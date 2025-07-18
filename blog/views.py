81d305e5deeb029b71b676d222863dc556dc61d5
name-amir <name.amir82@gmail.com>
2025-07-18 19:40:41 +0330

    filter posts with authors

--- blog/views.py
+++ blog/views.py
@@ -6,10 +6,12 @@ from blog.models import Post
 
 # Create your views here.
 
-def blog_view(request,cat_name=None):
+def blog_view(request,**kwargs):
     posts = Post.objects.filter(status=1)
-    if cat_name:
-        posts = posts.filter(category__name=cat_name)
+    if kwargs.get('cat_name') != None:
+        posts = posts.filter(category__name=kwargs['cat_name'])
+    if kwargs.get('author_username') != None:
+        posts = posts.filter(author__username=kwargs['author_username'])
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html', context)
 
